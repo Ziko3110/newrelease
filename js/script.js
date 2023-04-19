0a7b420a00ee4f7a62d5efca3e473a79d674e133
Ziko <example@gmail.com>
2023-04-19 22:43:05 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+/*const answer = [];
+answer[0] = prompt ('name',' ');
+answer[1] = prompt ('lastname','');
+answer[2] = prompt ('how old are u','');
+
+document.write(answer);
+*/
+const user = 'Ivan';
+
+alert (`привет, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*a = 15;
+console.log(a);
+
+
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+{
+    var result = 50;
+}
+console.log(result);
+/*
+
+
+const storeName = 'kixx';
+const storeDescription ={
+    budget: 10000,
+    employees: [ 'Ivan', 'Balvan','Baran' ],
+    products: {
+        'name': 1,
+        'price': 103
+    },
+    
+    open: true    
+}
+console.log(storeDescription['products']);*/
